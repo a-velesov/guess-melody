d3f6ecb68dfe43b5c2272157cfbff322050b2ad5
Alex <alexnem@inbox.ru>
2020-08-01 03:12:07 +0300

    feat: implement random question

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,22 +1,50 @@
 import React from 'react';
-import { QuizQuestion } from './QuizQuestion/QuizQuestion';
-import { AnswerList } from './AnswerList/AnswerList';
-import { AnswerDetails } from './AnswerDetails/AnswerDetails';
 import './ActiveQuiz.css';
+import { QuizQuestion } from './QuizQuestion/QuizQuestion';
+import { NavBar } from '../Navigation/NavBar';
+import { AnswerBlock } from './AnswerList/AnswerBlock';
 
 export const ActiveQuiz = (props) => {
+
+  const randomQuestion = () => {
+    let idx = Math.floor(Math.random() * Math.floor(6));
+    return props.questions[idx]
+  }
+
+/*  { props.questions.map((key, i) => {
+
+    /!*          const questionItem = Object.entries(key).forEach(([k, ind]) => {
+     return console.log(k ,ind)
+     })*!/
+
+
+    //console.log(questionItem[idx]);
+    console.log('key', key);
+
+
+    return <QuizQuestion
+      id={key.id}
+      name={key.name}
+      species={key.species}
+      image={key.image}
+      audio={key.audio}
+    />
+  })
+  }*/
+
   return (
     <>
-      <QuizQuestion />
+      <NavBar />
+
+      <QuizQuestion question={randomQuestion()} />
 
       <div className='answer-blocks row'>
 
-      <AnswerList />
-      <AnswerDetails />
+      <AnswerBlock answer={props.questions} />
+
 
       </div>
       <button className='btn'>Next</button>
-
     </>
   );
 };
--- src/components/ActiveQuiz/QuizQuestion/QuizQuestion.js
+++ src/components/ActiveQuiz/QuizQuestion/QuizQuestion.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import AudioPlayer from 'react-h5-audio-player';
-import './../Player/Player.scss'
-import './QuizQuestion.css'
+import './../Player/Player.scss';
+import './QuizQuestion.css';
 
 export const QuizQuestion = (props) => {
+
   return (
     <div className='random-bird jumbotron rounded'>
-      <img className='bird-image' src="https://via.placeholder.com/200x155.png" alt="Bird" />
+      <img className='bird-image' src={ props.question.image } alt="Bird" />
        <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <h2>*****</h2>
             </li>
             <li className="list-group-item">
-              <AudioPlayer layout='horizontal' showJumpControls={false} src="https://www.xeno-canto.org/sounds/uploaded/VOLIQOYWKG/XC489247-190724_09.10h_huiszwaluw_biesbosch_amaliahoeve_roep_100%2Bex_fouragerend_gezien_%20%282%29.mp3" />
+              <AudioPlayer layout='horizontal' showJumpControls={ false } src="https://www.xeno-canto.org/sounds/uploaded/VOLIQOYWKG/XC489247-190724_09.10h_huiszwaluw_biesbosch_amaliahoeve_roep_100%2Bex_fouragerend_gezien_%20%282%29.mp3" />
             </li>
        </ul>
     </div>
